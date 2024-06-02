0d6e24bbf039022470a5e4b7e7644ca126e102bd
Nimish Kumar <nimishk642@gmail.com>
2024-06-02 23:20:37 +0530

    Added custom application.properties and showed its usage

--- src/main/java/com/example/learning/RestController/FunRest.java
+++ src/main/java/com/example/learning/RestController/FunRest.java
@@ -1,14 +1,28 @@
 package com.example.learning.RestController;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRest {
 
+    @Value("${developer.name}")
+    private String developerName;
+
+    @Value("${developer.company}")
+    private String developerCompany;
+
+
     @GetMapping("/hello")
     private String helloWorld(){
         return "Hell World!!";
     }
+
+    @GetMapping("/devinfo")
+    private  String getDevInfo(){
+        return (developerName +" works in " +developerCompany);
+    }
+
 }
